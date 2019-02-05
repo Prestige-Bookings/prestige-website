d04db28627af32c34b027ac3a2b78c8de6eb25f5
Draxs <draxs75@gmail.com>
2019-02-05 18:18:24 +0200

    update

--- grunt/imagemin.js
+++ grunt/imagemin.js
@@ -2,15 +2,15 @@ var mozjpeg = require('imagemin-mozjpeg');
 module.exports = {
     dynamic: { 
       options: {
-        optimizationLevel: 7,
+        optimizationLevel: 0,
         progressive: false,
         svgoPlugins: [{ removeViewBox: false }],
-        use: [mozjpeg({fastCrush: true, quality: 100, smooth: 100})]
+        use: [mozjpeg({fastCrush: false, quality: 100})]
       },
         files: [{
           expand: true,
           cwd: 'src/assets/',
-          src: ['images/*.{png,jpg,jpeg,gif}'],
+          src: ['images/**/*.{png,jpg,jpeg,gif}'],
           dest: 'dist/assets'
         }]
       }
